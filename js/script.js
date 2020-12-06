e49950fc7573e7c6dba3318852b6c63ed3359f24
rubentrottein <ruben.trottein@gmail.com>
2020-12-06 02:24:21 +0100

    MAJ Charte graphique et sticky nav

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ function capFirst(element){
 for (let i=0; i<sections.length;i++){
     nav.innerHTML+= `<li><a href="#`+sections[i].id+`">`+capFirst(sections[i].id)+`</a></li>`;
 }
+
+
+//Sticky Nav
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the offset position of the navbar
+var sticky = nav.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
